3c6185c4269790cddcc99a1b50233f03a2418fa3
anshumoni <manisha.keshari@gmail.com>
2024-02-15 11:59:59 +0530

    mongoose crud operayion

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-const dbconnect = require("./connect")
+const mongoose = require("mongoose")
 
-const main = async()=>{
-    let collection = await dbconnect();
-    let response = await collection.find().toArray()
-    console.warn("res",response);
+mongoose.connect("mongodb://localhost:27017/ecomerce")
+
+const productSchema = new mongoose.Schema({
+    name:String,
+    price:Number,
+    brand:String,
+    category:String
+})
+
+const saveToDB = async()=>{
+    const ProductModel = new mongoose.model('products',productSchema);
+    const data = new ProductModel({
+        name:"Sony",
+        price:4000,
+        brand:"Sony",
+        category:"electronic"
+    })
+    const result = await data.save();
+    console.log("result",result);
+}
+ 
+const updatetoDB = async()=>{
+    const ProductModel = new mongoose.model('products',productSchema);
+    const data = await ProductModel.updateOne(
+            {name:'Sony'},
+        {
+            $set:{price:5000}
+        }
+        )
+     console.log(data)   
 }
 
-main();
+const deleteDb = async()=>{
+    const ProductModel = new mongoose.model('products',productSchema);
+    const data = await ProductModel.deleteOne({name:"Mobile"})
+    console.log("data",data);
+ 
+}
+
+const searchDB = async()=>{
+    const ProductModel = new mongoose.model('products',productSchema);
+    const data = await ProductModel.find({name:"Sony"})
+    console.log("srch result",data)
+
+}
+searchDB()
